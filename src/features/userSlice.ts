2ed95a3184a3fe1859dc60974ed8ec7bf18c9871
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-23 14:39:52 +0900

    create user redux

---
+++ src/features/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../app/store";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: { uid: "", photoUrl: "", displayName: "" },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+      //actionのpayloadから引き継いだuser情報を登録
+    },
+    logout: (state) => {
+      state.user = { uid: "", prhotoUrl: "", displayName: "" };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
