2c4ec1468e1d3ec414961ba99939e5e5c8a95792
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-23 16:44:43 +0900

    create logout button in feed component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { auth } from "../firebase";
 const Feed = () => {
-  return <div>Feeeeeeed!!!</div>;
+  return (
+    <div>
+      Feeeeeeed!!!
+      <button
+        onClick={() => {
+          auth.signOut();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Feed;
