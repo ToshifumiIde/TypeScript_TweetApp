341c0ff615d6850d68c31488dd5a71684be30269
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-23 15:14:09 +0900

    create login useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import React , { useEffect } from 'react';
-import styles from './App.module.css';
-import {useSelector , useDispatch } from "react-redux";
-import { selectUser , login ,logout } from "./features/userSlice";
-import 
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
-function App() {
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-  return (
-    <div className={styles.app}>
-      
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //userが存在する場合、userSliceで作成したloginのreducerをdispatch経由で実行
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoUrl,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return ()=>{
+      unSub();
+    }
+  }, [dispatch]);
+  return <div className={styles.app}></div>;
+};
 
 export default App;
