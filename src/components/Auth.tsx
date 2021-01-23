3c1c559c8e0a5503fe1cbd423aaf64de0e3073a2
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-23 20:54:18 +0900

    create username and pthoto url

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Auth.module.css";
 import { useDispatch } from "react-redux";
+import { updateUserProfile } from "../features/userSlice";
 import { auth, provider, storage } from "../firebase";
 
 import {
@@ -59,6 +60,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth: React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [username, setUserName] = useState<string>("");
@@ -93,6 +95,12 @@ const Auth: React.FC = () => {
       displayName: username,
       photoURL: url,
     });
+    dispatch(
+      updateUserProfile({
+        displayName: username,
+        photoUrl: url,
+      })
+    );
   };
   const signInGoogle = async () => {
     await auth.signInWithPopup(provider).catch((err) => alert(err.message));
