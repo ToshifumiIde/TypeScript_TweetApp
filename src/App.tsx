4e8cfd49f56cdb4c4a4c15385d21b604c42396b0
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-23 14:22:01 +0900

    create env and firebaseConfig

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-  console.log(process.env.REACT_APP_FOO);
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return (
     <div className="App">
       <header className="App-header">
