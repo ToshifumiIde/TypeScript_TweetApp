ec20f47354c76be271bc7b77f76baf1ebb89e583
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-24 09:24:59 +0900

    create user avatar in tweetinput

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
 const Feed = () => {
   return (
     <div>
       Feeeeeeed!!!
+      <TweetInput />
       <button
         onClick={() => {
           auth.signOut();
